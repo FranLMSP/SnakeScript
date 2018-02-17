89aafaa6806e60b56a0b453182a7e997c4b094f7
Franco Colmenarez <francolmenagar2@hotmail.com>
2018-02-17 15:17:46 -0400

    All keys are set

--- Input.js
+++ Input.js
@@ -11,16 +11,28 @@ class Input
 		// this.action();38 40 37 39
 		switch(event.keyCode)
 		{
-			case 38:
+			case 8:
+			case 74:
+				this.keyBack();
+				break;
+			case 13: //ENTER
+			case 75: //K
+				this.keyAction();
+				break;
+			case 38: //UP Arrow
+			case 87: //W
 				this.keyUp();
 				break;
-			case 40:
+			case 40: //Down Arrow
+			case 83: //S
 				this.keyDown();
 				break;
-			case 37:
+			case 37: //Left Arrow
+			case 65: //A
 				this.keyLeft();
 				break;
-			case 39:
+			case 39: //Right Arrow
+			case 68: //D
 				this.keyRight();
 				break;
 			default:
@@ -55,5 +67,15 @@ class Input
 	{
 		alert('Key RIGHT From Input Class');
 
+	}
+
+	keyAction()
+	{
+		alert('action key pressed');
+	}
+
+	keyBack()
+	{
+		alert('back key pressed');
 	}
 }
